c9eb0a33cb0e9e0f4f5af2469c4219b381e0b182
diespamer <diespamer@comcast.net>
2014-11-23 11:44:24 -0500

    cachematrix.R - my code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,78 @@
-## Put comments here that give an overall description of what your
-## functions do
+## rabornd 2014/11/23 - init
 
-## Write a short comment describing this function
+## Encapsulate a matrix and its inverse, cacheing the inverse on
+## first request to minimize inverse calculations
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix():
+##   Produces an object that stores a matrix and
+##   its inverse but does NOT calc the inverse.
+makeCacheMatrix <- function(x = matrix())
+{
+    # init var to hold inverse
+    i<-NULL
 
+    # function to set matrix
+    set<-function(y)
+    {
+        # x is stored in parent env.
+        x<<-y
+
+        # if we are calling this function then 
+        # we are using new matrix so reset inverse
+        # to null. i is stored in parent env.
+        i<<-NULL
+    }
+
+    # get matrix
+    get<-function()
+    {
+        x
+    }
+    
+    # set matrix's inverse
+    setinverse<-function(inv)
+    {
+        # i is stored in parent env.
+        i<<-inv
+    }
+
+    # get matrix's inverse
+    getinverse<-function()
+    {
+        i
+    }
+   
+    # functions of returned object 
+    list(
+        set=set,
+        get=get,
+        setinverse=setinverse,
+        getinverse=getinverse
+    )
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+## cacheSolve():
+##   Calculates and caches (if not already cached) and
+##   returns the inverse of a matrix stored in a
+##   makeCacheMatrix object.
+cacheSolve <- function(x, ...)
+{
         ## Return a matrix that is the inverse of 'x'
+
+        # get inverse value
+        i<-x$getinverse()
+
+        # if no inverse value, calc and cache
+        if(!is.null(i))
+        {
+            message("getting cached inverse")
+            return(i)
+        }
+        mat<-x$get()
+        i<-solve(mat)
+        x$setinverse(i)
+
+        # return inverse
+        i
 }
